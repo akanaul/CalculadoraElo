20c668f26fe548b370dedb66b7b5848f513860b4
akanaul <luanalves0512@gmail.com>
2025-09-16 17:51:11 -0300

    Esqueleto inicial

--- eloCalc.js
+++ eloCalc.js
@@ -1,1 +1,11 @@
 //Calculadora de elo simples
+let victoryNum = 5
+let defeatNum = 5
+let finalElo = calcElo(victoryNum, defeatNum)
+
+console.log("Seu elo é " + finalElo)
+
+function calcElo(victoryNum, defeatNum){
+    let calcElo = victoryNum - defeatNum
+    return calcElo
+}
